28097e9be0435b2462b1aab38f0e6cabcb8d67ef
ARIF MULANI <amnetwork049@gmail.com>
2023-12-04 19:21:42 +0530

    commented findlargestelement

--- assignments/01-js/easy/findLargestElement.js
+++ assignments/01-js/easy/findLargestElement.js
@@ -6,13 +6,21 @@
 */
 
 function findLargestElement(numbers) {
+  // Set the initial maximum element as the first element in the array
   let max_element = numbers[0];
+
+  // Iterate through the array starting from the second element
   for (let i = 1; i < numbers.length; i++) {
-    if(numbers[i] > max_element) {
-        max_element = numbers[i];
+    // Check if the current element is greater than the current maximum element
+    if (numbers[i] > max_element) {
+      // If true, update the maximum element to the current element
+      max_element = numbers[i];
     }
   }
+
+  // Return the maximum element found in the array
   return max_element;
 }
+
 
 module.exports = findLargestElement;
