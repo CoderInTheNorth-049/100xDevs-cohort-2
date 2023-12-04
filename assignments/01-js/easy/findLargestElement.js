25802e7916c29ac9ea11f71b6471d96128596870
ARIF MULANI <142739281+CoderInTheNorth-049@users.noreply.github.com>
2023-12-04 18:57:36 +0530

    Add files via upload
    easy/findlargestelement solved

---
+++ assignments/01-js/easy/findLargestElement.js
@@ -0,0 +1,18 @@
+/*
+  Write a function `findLargestElement` that takes an array of numbers and returns the largest element.
+  Example:
+  - Input: [3, 7, 2, 9, 1]
+  - Output: 9
+*/
+
+function findLargestElement(numbers) {
+  let max_element = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if(numbers[i] > max_element) {
+        max_element = numbers[i];
+    }
+  }
+  return max_element;
+}
+
+module.exports = findLargestElement;
