4f2bde96d34f82ea2b29654d90fcaabef6c940a7
ARIF MULANI <amnetwork049@gmail.com>
2023-12-06 22:03:48 +0530

    calculator solved

--- assignments/week-1/01-js/hard/calculator.js
+++ assignments/week-1/01-js/hard/calculator.js
@@ -16,6 +16,51 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0; // Initialize result variable in the constructor
+  }
+
+  // Methods for arithmetic operations
+  add(num) {
+    this.result += num; // Adds the given number to the result
+  }
+
+  subtract(num) {
+    this.result -= num; // Subtracts the given number from the result
+  }
+
+  multiply(num) {
+    this.result *= num; // Multiplies the result by the given number
+  }
+
+  divide(num) {
+    if (num === 0) {
+      throw new Error("Cannot divide by zero"); // Throws an error if division by zero is attempted
+    }
+    this.result /= num; // Divides the result by the given number
+  }
+
+  clear() {
+    this.result = 0; // Resets the result variable to 0
+  }
+
+  getResult() {
+    return this.result; // Returns the current value of the result variable
+  }
+
+  calculate(expression) {
+    try {
+      this.result = eval(expression); // Evaluates the expression and assigns the result
+    } catch (err) {
+      throw new Error("Invalid expression."); // Throws an error for an invalid expression
+    }
+    if (this.result === Infinity) {
+      throw new Error("Cannot divide a number by 0."); // Throws an error if division by zero occurs during evaluation
+    }
+    return this.result; // Returns the final calculated result
+  }
+}
 
 module.exports = Calculator;
+
