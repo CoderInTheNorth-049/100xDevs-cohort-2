c546feb09d8033760ebea73ece29aae6cd7014ea
ARIF MULANI <amnetwork049@gmail.com>
2023-12-06 12:00:41 +0530

    medium solved

--- assignments/01-js/medium/countVowels.js
+++ assignments/01-js/medium/countVowels.js
@@ -6,7 +6,21 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  // Convert the input string to lowercase to consider both uppercase and lowercase vowels
+  let s = str.toLowerCase();
+  let result = 0; // Initialize a variable to store the count of vowels
+
+  let vowels = ['a', 'e', 'i', 'o', 'u']; // Array containing lowercase vowels
+
+  for (let i = 0; i < s.length; i++) {
+      // Iterate through each character in the string
+      if (vowels.includes(s[i])) {
+          // Check if the current character is a vowel (present in the vowels array)
+          result += 1; // Increment the count of vowels when a vowel is found
+      }
+  }
+
+  return result; // Return the total count of vowels in the string
 }
 
 module.exports = countVowels;
--- assignments/01-js/medium/times.js
+++ assignments/01-js/medium/times.js
@@ -9,5 +9,21 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    const start = new Date(); // Record the current time before computation
+
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i; // Perform the sum calculation from 1 to n
+    }
+
+    const end = new Date(); // Record the current time after computation
+
+    const timeTaken = end - start; // Calculate the time difference in milliseconds
+    return timeTaken / 1000; // Convert milliseconds to seconds and return
 }
+
+// Test cases
+console.log(calculateTime(1000000)); // Time taken to calculate sum from 1-1e6
+console.log(calculateTime(100000000)); // Time taken to calculate sum from 1-1e8
+console.log(calculateTime(1000000000)); // Time taken to calculate sum from 1-1e9
+console.log(calculateTime(10000000000)); // Time taken to calculate sum from 1-1e10
