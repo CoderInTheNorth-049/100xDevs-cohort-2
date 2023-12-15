1ba8750bb0414323bfb9bc02f7e47e76d9bc7d79
ARIF MULANI <amnetwork049@gmail.com>
2023-12-15 17:36:43 +0530

    3-promise-all

--- assignments/week-2/01-async-js/hard (promises)/3-promise-all.js
+++ assignments/week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,40 @@
  */
 
 function wait1(t) {
-
-}
-
-function wait2(t) {
-
-}
-
-function wait3(t) {
-
-}
-
-function calculateTime(t1, t2, t3) {
-
-}
-
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(`Waited for ${t} seconds`);
+      }, t * 1000);
+    });
+  }
+  
+  function wait2(t) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(`Waited for ${t} seconds`);
+      }, t * 1000);
+    });
+  }
+  
+  function wait3(t) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(`Waited for ${t} seconds`);
+      }, t * 1000);
+    });
+  }
+  
+  function calculateTime(t1, t2, t3) {
+    const startTime = Date.now();
+  
+    const promise1 = wait1(t1);
+    const promise2 = wait2(t2);
+    const promise3 = wait3(t3);
+  
+    return Promise.all([promise1, promise2, promise3]).then(() => {
+      const endTime = Date.now();
+      return endTime - startTime; // Returning the time taken in milliseconds
+    });
+  }
+    
 module.exports = calculateTime;
