f748d450233933d5a8edae6a6daa3e73949a94ef
ARIF MULANI <amnetwork049@gmail.com>
2023-12-15 17:20:50 +0530

    2-sleep-completely

--- assignments/week-2/01-async-js/hard (promises)/2-sleep-completely.js
+++ assignments/week-2/01-async-js/hard (promises)/2-sleep-completely.js
@@ -5,6 +5,15 @@
  */
 
 function sleep(milliseconds) {
+    return new Promise(resolve => {
+        const startTime = Date.now();
+        while (Date.now() - startTime < milliseconds) {
+      // Busy-wait loop: This loop will continuously run until the specified milliseconds have passed
+      // During this time, the thread is occupied and unable to perform other tasks
+      // It simulates a thread being 'busy' or 'blocked'
+        }
+        resolve(); // Resolve the promise after the specified time
+    })
 }
 
 module.exports = sleep;
