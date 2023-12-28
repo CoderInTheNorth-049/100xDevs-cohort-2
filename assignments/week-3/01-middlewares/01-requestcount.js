9a9888aea876e9e945e023bc68889b7f6cec69fc
ARIF MULANI <amnetwork049@gmail.com>
2023-12-28 23:04:43 +0530

    02-ratelimitter.js solved

--- assignments/week-3/01-middlewares/01-requestcount.js
+++ assignments/week-3/01-middlewares/01-requestcount.js
@@ -1,3 +1,8 @@
+// You have been given an express server which has a few endpoints.
+// Your task is to create a global middleware (app.use) which will
+// maintain a count of the number of requests made to the server in the global
+// requestCount variable
+
 const request = require('supertest');
 const assert = require('assert');
 const express = require('express');
@@ -5,26 +10,25 @@ const express = require('express');
 const app = express();
 let requestCount = 0;
 
-// You have been given an express server which has a few endpoints.
-// Your task is to create a global middleware (app.use) which will
-// maintain a count of the number of requests made to the server in the global
-// requestCount variable
-
+// Global middleware to increment the request count for every incoming request
 app.use((req, res, next) => {
-  requestCount+=1;
-  next();
+  requestCount += 1; // Incrementing the request count for each incoming request
+  next(); // Passing control to the next middleware or route handler
 });
 
+// Endpoint to handle GET requests for user information
 app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+  res.status(200).json({ name: 'john' }); // Sending a JSON response with user data
 });
 
+// Endpoint to handle POST requests for creating a dummy user
 app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+  res.status(200).json({ msg: 'created dummy user' }); // Sending a JSON response for user creation
 });
 
+// Endpoint to retrieve the current request count
 app.get('/requestCount', function(req, res) {
-  res.status(200).json({ requestCount });
+  res.status(200).json({ requestCount }); // Sending the current request count as a JSON response
 });
 
-module.exports = app;
+module.exports = app; // Exporting the Express app
--- assignments/week-3/01-middlewares/02-ratelimitter.js
+++ assignments/week-3/01-middlewares/02-ratelimitter.js
@@ -1,7 +1,3 @@
-const request = require('supertest');
-const assert = require('assert');
-const express = require('express');
-const app = express();
 // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
 // rate limit the requests from a user to only 5 request per second
@@ -10,18 +6,48 @@ const app = express();
 // User will be sending in their user id in the header as 'user-id'
 // You have been given a numberOfRequestsForUser object to start off with which
 // clears every one second
+// Import required modules and set up the Express app
 
+const request = require('supertest'); // Importing Supertest for testing
+const assert = require('assert'); // Importing assertion library
+const express = require('express'); // Importing Express.js
+const app = express(); // Creating an instance of Express app
+
+// Object to track the number of requests made by each user within a second
 let numberOfRequestsForUser = {};
+
+// Resetting the request count for each user every second
 setInterval(() => {
     numberOfRequestsForUser = {};
-}, 1000)
+}, 1000);
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+// Middleware to enforce rate limiting
+app.use((req, res, next) => {
+    const user = req.headers["user-id"]; // Extracting user ID from request headers
+    if (numberOfRequestsForUser[user]) {
+        // If user has made requests within the last second
+        numberOfRequestsForUser[user] += 1;
+        if (numberOfRequestsForUser[user] > 5) {
+            // If user has exceeded the rate limit of 5 requests per second
+            res.status(404).send("Rate limit exceeded"); // Blocking the request with a 404 response
+            return;
+        }
+        next(); // Allowing the request to proceed
+    } else {
+        // If it's the first request from the user within the last second
+        numberOfRequestsForUser[user] = 1; // Initializing the request count for the user
+        next(); // Allowing the request to proceed
+    }
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+// Sample endpoint - GET request to retrieve user information
+app.get('/user', function (req, res) {
+    res.status(200).json({ name: 'john' }); // Sending a JSON response with user data
 });
 
-module.exports = app;
+// Sample endpoint - POST request to create a dummy user
+app.post('/user', function (req, res) {
+    res.status(200).json({ msg: 'created dummy user' }); // Sending a JSON response for user creation
+});
+
+module.exports = app; // Exporting the Express app
