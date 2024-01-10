924c8a937b790e6a54fd6dd9527d8bcee3872453
ARIF MULANI <amnetwork049@gmail.com>
2024-01-10 15:28:53 +0530

    conflict resolved

--- assignments/week-4/react-basics/Todo-app/backend/db.js
+++ assignments/week-4/react-basics/Todo-app/backend/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 // mongodb+srv://admin:arif049@cluster0.tfqnzyt.mongodb.net/todos
 mongoose.connect("mongodb+srv://admin:arif049@cluster0.tfqnzyt.mongodb.net/todos");
@@ -13,20 +12,4 @@ const todo = mongoose.model('todos', todoSchema);
 
 module.exports = {
     todo
-=======
-const mongoose = require('mongoose');
-// mongodb+srv://admin:arif049@cluster0.tfqnzyt.mongodb.net/todos
-mongoose.connect("mongodb+srv://admin:arif049@cluster0.tfqnzyt.mongodb.net/todos");
-
-const todoSchema = mongoose.Schema({
-    title: String,
-    description: String,
-    completed: Boolean
-})
-
-const todo = mongoose.model('todos', todoSchema);
-
-module.exports = {
-    todo
->>>>>>> d4720f280b6c8f20ec47f3fde716e914016b81db
 }
