c5945147cb5cdf0cba5f5622ea349bd7267aff2b
ARIF MULANI <amnetwork049@gmail.com>
2024-01-10 15:16:39 +0530

    Todo-app almost complete

--- assignments/week-4/react-basics/Todo-app/backend/index.js
+++ assignments/week-4/react-basics/Todo-app/backend/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const bodyParser = require('bodyParser');
-const { createTodo } = require('./types');
-
+const { createTodo, updateTodo } = require('./types');
+const { todo } =require('./db');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.post('/todo', (req, res) => {
+app.post('/todo', async (req, res) => {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
 
@@ -18,15 +19,27 @@ app.post('/todo', (req, res) => {
         return;
     }
 
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
+    res.json({
+        msg: "Todo created"
+    })
+
 })
 
-app.get('/todos', (req, res) => {
-
+app.get('/todos', async (req, res) => {
+    const todos = await todo.find({});
+    res.json({
+        todos
+    })
 })
 
-app.put('/completed', (req, res) => {
-    const createPayload = req.body;
-    const parsedPayload = createTodo.safeParse(createPayload);
+app.put('/completed', async (req, res) => {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
 
     if(!parsedPayload.success){
         res.status(411).json({
@@ -34,6 +47,15 @@ app.put('/completed', (req, res) => {
         })
         return;
     }
+
+    await todo.updateOne({
+        _id: req.body.id
+    }, {
+        completed: true
+    })
+    res.json({
+        msg: "Todo marked as completed"
+    })
 })
 
 app.listen(3000);
