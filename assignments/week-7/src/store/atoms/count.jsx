42f71639dec605fa8ee817766933adbd42e95273
ARIF MULANI <amnetwork049@gmail.com>
2024-06-02 16:56:42 +0530

    week-7 Recoil

--- assignments/week-7/src/store/atoms/count.jsx
+++ assignments/week-7/src/store/atoms/count.jsx
@@ -1,14 +1,16 @@
-import {atom, selector} from "recoil"
+import { atom, selector } from "recoil"; // Importing atom and selector from Recoil
 
+// Defining an atom to hold the count state
 export const countAtom = atom({
-    key: "countAtom",
-    default: 0
+    key: "countAtom", // unique ID for this atom
+    default: 0 // initial value of the atom
 });
 
+// Defining a selector to compute if the count is even
 export const isEvenSelector = selector({
-    key: 'isEvenselector',
-    get: ({get}) => {
-        const count=get(countAtom)
-        return count%2===0
+    key: 'isEvenSelector', // unique ID for this selector
+    get: ({ get }) => {
+        const count = get(countAtom); // getting the current value of countAtom
+        return count % 2 === 0; // returning true if count is even, false otherwise
     }
-})
+});
