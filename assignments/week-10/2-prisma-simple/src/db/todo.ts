e28c3d5160158d430eb234c1528e84f77e98d7bc
ARIF MULANI <amnetwork049@gmail.com>
2025-03-15 12:50:15 +0530

    prisma assignment completed

--- assignments/week-10/2-prisma-simple/src/db/todo.ts
+++ assignments/week-10/2-prisma-simple/src/db/todo.ts
@@ -13,7 +13,15 @@ const prisma = new PrismaClient();
  * }
  */
 export async function createTodo(userId: number, title: string, description: string) {
-    
+    const todo = await prisma.todo.create({
+      data: {
+        title,
+        description,
+        done: false,
+        userId
+      }
+    })
+    return todo;
 }
 /*
  * mark done as true for this specific todo.
@@ -26,7 +34,15 @@ export async function createTodo(userId: number, title: string, description: str
  * }
  */
 export async function updateTodo(todoId: number) {
-
+    const todo = await prisma.todo.update({
+      where: {
+        id: todoId
+      },
+      data: {
+        done: true
+      }
+    })
+    return todo;
 }
 
 /*
@@ -40,5 +56,10 @@ export async function updateTodo(todoId: number) {
  * }]
  */
 export async function getTodos(userId: number) {
-
+  const todos = await prisma.todo.findMany({
+    where: {
+      userId: userId
+    }
+  })
+  return todos;
 }
--- assignments/week-10/2-prisma-simple/src/db/user.ts
+++ assignments/week-10/2-prisma-simple/src/db/user.ts
@@ -13,7 +13,14 @@ const prisma = new PrismaClient();
  * }
  */
 export async function createUser(username: string, password: string, name: string) {
-    
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password,
+        name
+      }
+    })
+    return user;
 }
 
 /*
@@ -25,5 +32,10 @@ export async function createUser(username: string, password: string, name: strin
  * }
  */
 export async function getUser(userId: number) {
-    
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId
+      }
+    })
+    return user;
 }
